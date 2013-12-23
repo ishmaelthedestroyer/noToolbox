db61c9939cb3aefc62287d5932a201d0a4b3fb0a
Ishmael tD <ishmaelthedestroyer@gmail.com>
2013-12-23 17:15:53 -0600

    added bxSluggify callback

--- dist/lib/directives/bxSluggify/lib/bxSluggify.js
+++ dist/lib/directives/bxSluggify/lib/bxSluggify.js
@@ -1,12 +1,18 @@
 angular.module('bxSluggify', []).directive('bxsluggify', function($document) {
   return function(scope, element, attr) {
-    var sluggify;
+    var cb, sluggify;
+    cb = function() {
+      return scope.$apply(function() {
+        return scope.$eval(attr.bxsluggify);
+      });
+    };
     sluggify = function(text) {
       return text.toLowerCase().replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-');
     };
     return element.bind('keyup', function() {
       var slug;
       slug = sluggify(element.val());
+      cb();
       return scope.$apply(element.val(slug));
     });
   };
--- lib/directives/bxSluggify/lib/bxSluggify.coffee
+++ lib/directives/bxSluggify/lib/bxSluggify.coffee
@@ -2,11 +2,17 @@ angular.module('bxSluggify', [])
 
 .directive 'bxsluggify', ($document) ->
   (scope, element, attr) ->
+    cb = () ->
+      scope.$apply ->
+        scope.$eval attr.bxsluggify
+
     sluggify = (text) ->
       return text.toLowerCase()
       .replace(/[^a-z0-9 -]/g, '').replace(/\s+/g, '-')
       .replace(/-+/g, '-')
 
+
     element.bind 'keyup', () ->
-      slug = sluggify element.val()
-      scope.$apply element.val slug
+      slug = sluggify element.val() # make slug
+      cb() # fire callback
+      scope.$apply element.val slug # set slug
